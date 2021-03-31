1c6337b56fe56d52fb7a2f71bf9f5c465a8c8e8c
axemac <74874171+ax-lab@users.noreply.github.com>
2021-03-31 17:39:08 -0300

    Scaffold web application
    - Basic React setup
    - Move static files to `public` directory

---
+++ app/components/main/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => (
+	<>
+		<h1>Kotoba App</h1>
+	</>
+)
+
+export default Main
--- web/index.ts
+++
@@ -1,4 +0,0 @@
-import { answer } from './src/lib'
-
-console.log(`Javascript loaded - version 0.1.0`)
-console.log(`${answer()}`)

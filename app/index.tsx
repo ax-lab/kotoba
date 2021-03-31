21246e1641516c3391531d6a712dfb7cc7fd509e
axemac <74874171+ax-lab@users.noreply.github.com>
2021-03-31 20:11:05 -0300

    Backend access test code

--- app/index.tsx
+++ app/index.tsx
@@ -4,3 +4,10 @@ import ReactDOM from 'react-dom'
 import App from './app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+fetch('/api')
+	.then((data) => data.json())
+	.then((data) => {
+		console.log('API:', data)
+	})
+	.catch((err) => console.error(err))

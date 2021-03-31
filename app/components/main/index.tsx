2403aba19cec781d69731e77bcf461f2619f1c9a
axemac <74874171+ax-lab@users.noreply.github.com>
2021-03-31 20:45:58 -0300

    Implement basic React routing

--- app/components/main/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Main = () => (
-	<>
-		<h1>Kotoba App</h1>
-	</>
-)
-
-export default Main
---
+++ app/components/video/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Video = () => (
+	<>
+		<h2>Video</h2>
+	</>
+)
+
+export default Video

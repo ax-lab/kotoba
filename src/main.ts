4f62acf3842a905b442b20b4ad1565689119c185
axemac <74874171+ax-lab@users.noreply.github.com>
2021-03-31 13:59:03 -0300

    Encapsulate MPV player control

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { version } from './lib'
-import * as mpv from './mpv'
+import { MPV } from './mpv'
 
 async function main() {
-	await mpv.open()
+	const player = MPV.get()
+
+	player.on('connect', () => {
+		console.log('IPC connected')
+		player.send_command({ command: ['request_log_messages', 'debug'], async: true })
+	})
+
+	player.on('disconnect', () => console.log('IPC disconnected'))
+
+	player.on('data', (data) => console.log('RECV', JSON.stringify(data)))
+
+	player.on('output', (line) => console.log(`OUT: ${line}`))
+
+	player.on('error', (line) => console.error(`ERR: ${line}`))
+
+	await player.open()
 }
 
 main().catch((err) => {

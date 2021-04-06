7f7fcac0a156fd1a1da638caf63a72e0b461509c
axemac <74874171+ax-lab@users.noreply.github.com>
2021-04-06 19:11:44 -0300

    Add subtitle toolbar
    - Implement "Copy to Clipboard" and "Translate"
    - Add buttons for playback synchronization and editing

--- app/views/subtitles.tsx
+++ app/views/subtitles.tsx
@@ -16,12 +16,12 @@ events.register((ev) => {
 })
 
 const SubtitleView = () => {
-	const [subtitle, setSubtitle] = useState(current_subtitle)
+	const [subtitle, set_subtitle] = useState(current_subtitle)
 
 	useEffect(() => {
 		const unregister = events.register((ev) => {
 			if (ev.type == 'subtitle-change') {
-				setSubtitle(ev)
+				set_subtitle(ev)
 			}
 		})
 		return () => unregister()
@@ -36,15 +36,92 @@ const SubtitleView = () => {
 	)
 }
 
-const Dialog = ({ entry }: { entry: SubtitleDialog }) => (
-	<div className="subtitle-entry">
-		<div className="time-label">
-			<TimeLabel time={entry.start.time} />
-			<TimeLabel time={entry.end.time} />
+const Dialog = ({ entry }: { entry: SubtitleDialog }) => {
+	const [popup, set_popup] = useState(false)
+	const popup_el = React.createRef<HTMLDivElement>()
+	useEffect(() => {
+		const fn = () => {
+			set_popup(false)
+		}
+		document.addEventListener('click', fn)
+		return () => {
+			document.removeEventListener('click', fn)
+		}
+	})
+
+	const copy = () => {
+		try {
+			void navigator.clipboard.writeText(entry.text)
+		} catch (e) {
+			// ignore
+		}
+	}
+
+	const translate = () => {
+		const encoded = window.encodeURIComponent(entry.text)
+		window.open(`https://www.deepl.com/translator#ja/en/${encoded}`, '_blank')
+	}
+
+	return (
+		<div className="subtitle-entry">
+			<div className="time-label">
+				<TimeLabel time={entry.start.time} />
+				<TimeLabel time={entry.end.time} />
+			</div>
+			<div className="subtitle-text">{Japanese(entry.text)}</div>
+			<div className="subtitle-toolbar">
+				<button className="far fa-clipboard" title="Copy to Clipboard" onClick={copy} />
+				<button className="fas fa-sync-alt" title="Loop Dialog" />
+				<button
+					className="fas fa-bars"
+					onClick={(ev) => {
+						const btn = (ev.target as Element).getBoundingClientRect()
+						const el = popup_el.current
+						if (el) {
+							const size = el.getBoundingClientRect()
+							const style = window.getComputedStyle(el)
+							const padR = parseFloat(style.paddingRight)
+							const padT = parseFloat(style.paddingTop)
+							el.style.left = `${btn.x - size.width - 2 * padR}px`
+							el.style.top = `${btn.y - padT}px`
+							// We need a timeout here because of the global click
+							// handler that dismisses the menu
+							setTimeout(() => {
+								set_popup(!popup)
+							}, 0)
+						}
+					}}
+				/>
+				<div ref={popup_el} className="popup" style={{ visibility: popup ? 'visible' : 'hidden' }}>
+					<button className="fas fa-globe" title="Translate" onClick={translate} />
+					<span className="separator" />
+					<button className="fas fa-step-forward" title="Set Loop B" />
+					<button className="fas fa-step-backward" title="Set Loop A" />
+					<span className="separator" />
+					<button
+						className="far fa-hourglass"
+						title="Sync All&#013;Syncs subtitle and apply delta to all subtitles"
+					/>
+					<button
+						className="fas fa-hourglass-end"
+						title="Sync Forward&#013;Syncs subtitle and apply delta to all subtitles going forward"
+					/>
+					<button
+						className="fas fa-stopwatch"
+						title="Sync Current&#013;Syncs subtitle to start at current playback position"
+					/>
+					<button
+						className="far fa-clock"
+						title="Sync Duration&#013;Changes subtitle duration to end at current playback position"
+					/>
+					<span className="separator" />
+					<button className="far fa-edit" title="Edit" />
+					<button className="far fa-trash-alt" title="Delete" />
+				</div>
+			</div>
 		</div>
-		<div className="subtitle-text">{Japanese(entry.text)}</div>
-	</div>
-)
+	)
+}
 
 const TimeLabel = ({ time }: { time?: number }) => {
 	if (!time) return <span />

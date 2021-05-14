b07162ba219936a45d664307a02c0ddc6a94d053
axemac <74874171+ax-lab@users.noreply.github.com>
2021-05-14 18:25:09 -0300

    Include XML parsing library

--- importer/import.ts
+++ importer/import.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 
+import sax from 'sax'
+
 import * as lib from '../lib'
 
 import { open_zip } from './util'
@@ -29,6 +31,46 @@ async function main() {
 	const zip = await open_zip(path.join(DATA_DIR, DICT_FILE))
 	const data = await zip.files['data.xml'].async('string')
 	console.log(`Loaded data.xml with ${lib.bytes(data.length)} in ${lib.elapsed(start)}`)
+
+	const strict = true
+	const parser = sax.parser(strict)
+
+	// Process the first part of the file for entities. Entities have the
+	// format `<!ENTITY hob "Hokkaido-ben">`
+	const matches = data.slice(0, 256 * 1024).matchAll(/<!ENTITY\s+([-\w]+)\s+"([^"]+)"\s*>/g)
+	const tags: Record<string, string> = {}
+	for (const it of matches) {
+		const [, entity, label] = it
+		tags[entity] = label
+		parser.ENTITIES[entity] = `{{${entity}}}`
+	}
+
+	// This is present on the last entry, but undeclared.
+	parser.ENTITIES['unc'] = '{{unc}}'
+
+	const max_events = 100
+	let count = 0
+
+	parser.onopentag = (node) => {
+		if (count > max_events) return
+		console.log(node)
+		count++
+	}
+
+	parser.onclosetag = (node) => {
+		if (count > max_events) return
+		console.log('CLOSE', node)
+	}
+
+	parser.ontext = (text) => {
+		if (count > max_events) return
+		console.log('TEXT', text)
+	}
+
+	const start_xml = lib.now()
+	parser.write(data)
+	parser.close()
+	console.log(`Processed XML in ${lib.elapsed(start_xml)}`)
 }
 
 main().catch((err) => {

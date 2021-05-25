9577af318ee3070369999327953a24e8d1802716
axemac <74874171+ax-lab@users.noreply.github.com>
2021-05-25 11:39:46 -0300

    Implement pitch loading

--- data/source/accents.md
+++ data/source/accents.md
@@ -8,10 +8,18 @@ tab-separated list of values with the following fields:
 - Word: the main entry. This is usually the kanji form for the word, except
   for words that only have a kana reading.
 - Read: the hiragana reading for the word. For kana-only words this is empty.
-- Pitch: comma-separated list of integers representing the pitch position.
+- Pitch: comma-separated list of optional tag and integers representing the 
+  pitch position:
 	- 0: all except the first mora are high.
 	- 1: all except the first mora are low.
 	- N: the first mora is low, all mora up to N are high, N is low.
+- The tag if present is written in between parenthesis before the number as a 
+  semi-colon separated list. Valid tags and their respective JMDict name are:
+	- `副` = `adv` ~ adverb
+	- `名` = `n` ~ noun
+	- `代` = `pn` ~ pronoun
+	- `形動` = `adj-na` ~ adjectival nouns or quasi-adjectives
+	- `感` = `int` ~ interjection
 
 For a reference on the pitch position see https://tejashdatta.medium.com/what-is-pitch-accent-in-japanese-and-is-it-important-ce1e60c65db3:
 
@@ -19,6 +27,9 @@ For a reference on the pitch position see https://tejashdatta.medium.com/what-is
 
 Reference for the Yomichan algorithm:
 - https://github.com/FooSoft/yomichan/blob/be23acf499bcdb530c1b8990aa3d6211323cbdb2/ext/js/display/display-generator.js#L631
+
+Reference for importing:
+- https://github.com/toasted-nutbread/yomichan-pitch-accent-dictionary/blob/master/main.js
 
 Source:
 	- Project: https://github.com/mifunetoshiro/kanjium
---
+++ importer/pitch.ts
@@ -0,0 +1,64 @@
+import * as path from 'path'
+
+import * as lib from '../lib'
+
+import { read_lines } from './files'
+
+const PITCH_FILE = 'accents.txt'
+
+type PitchInfo = Record<
+	string,
+	Record<
+		string,
+		Array<{
+			tags: string[]
+			pitch: number
+		}>
+	>
+>
+
+export async function import_pitch(source_dir: string) {
+	const start = lib.now()
+	const lines = (await read_lines(path.join(source_dir, PITCH_FILE)))
+		.filter((x) => !!x.trim())
+		.map((x) => x.split(/\t/))
+
+	const tag_map: Record<string, string> = {
+		副: `adv`,
+		名: `n`,
+		代: `pn`,
+		形動: `adj-na`,
+		感: `int`,
+	}
+
+	const pitch: PitchInfo = {}
+	let line = 1
+	for (const [word, read, list] of lines) {
+		const pitches = list.split(',').map((x) => {
+			const [tag_list, value] = x.startsWith('(') ? x.slice(1).split(')') : ['', x]
+			const tags = tag_list
+				? tag_list.split(';').map((tag) => {
+						if (tag && !tag_map[tag]) {
+							throw new Error(`invalid tag: ${tag} at ${line}: ${word} ${read} ${list}`)
+						}
+						return tag_map[tag]
+				  })
+				: []
+
+			const pitch = parseInt(value, 10)
+			if (typeof pitch != 'number' || isNaN(pitch)) {
+				throw new Error(`invalid pitch: ${x} at ${line}: ${word} ${read} ${list}`)
+			}
+			return { tags, pitch }
+		})
+		pitch[word] = pitch[word] || {}
+		pitch[word][read] = pitches
+		line++
+	}
+
+	console.log(`Loaded pitch information for ${lines.length} entries in ${lib.elapsed(start)}`)
+
+	console.log(JSON.stringify(pitch['月']))
+	console.log(JSON.stringify(pitch['９日']))
+	return pitch
+}
